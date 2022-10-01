76eeae8caa0f704339e7c4e3bb34e6d431f63b86
jperdomo <jperdomo3@gmail.com>
2022-09-30 20:20:05 -0400

    proxmox

---
+++ proxmox/setup-proxmox.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+echo "deb http://download.proxmox.com/debian buster pve-no-subscription" >> /etc/apt/sources.list
+echo "#deb https://enterprise.proxmox.com/debian/pve buster pve-enterprise" > /etc/apt/sources.list.d/pve-enterprise.list
+apt update && apt dist-upgrade -y
+bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
