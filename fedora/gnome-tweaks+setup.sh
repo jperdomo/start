772143d6305d9c0537fad158ecd126162036966d
jperdomo <jperdomo3@gmail.com>
2022-10-01 19:08:28 -0400

    time set

--- fedora/gnome-tweaks+setup.sh
+++ fedora/gnome-tweaks+setup.sh
@@ -6,3 +6,12 @@ sudo dnf install -y chrome-gnome-shell
 #Extension Manager
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.mattjakeman.ExtensionManager
+#12hr Clock
+gsettings set org.gnome.desktop.interface clock-format '12h'
+#Wallpaper
+##Broken :(
+#gsettings set org.gnome.desktop.background picture-uri ""
+#gsettings set org.gnome.desktop.background primary-color '#333333'
+#gsettings set org.gnome.desktop.background color-shading-type 'solid'
+#Dock
+#Dark Mode
