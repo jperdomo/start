afc4f93cc840e3d075a45d51868afc3d920bddf2
jperdomo <jperdomo3@gmail.com>
2022-09-30 20:58:11 -0400

    remove sudo

--- proxmox/install-tailscale.sh
+++ proxmox/install-tailscale.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-sudo apt install -y update
-sudo apt install -y tailscale
-sudo tailscale up
+install -y update
+install -y tailscale
+tailscale up
