f24a18ad32c5cb2544ecf65ad02db41e1de24101
jperdomo <jperdomo3@gmail.com>
2022-10-02 16:58:52 -0400

    removed chrome shell

--- fedora/gnome-tweaks+setup.sh
+++ fedora/gnome-tweaks+setup.sh
@@ -1,8 +1,10 @@
 #/bin/bash
-#Gnome tweaks + chrome gnome shell
+#Gnome tweaks
 sudo dnf install -y gnome-tweaks
-sudo dnf copr -y enable region51/chrome-gnome-shell
-sudo dnf install -y chrome-gnome-shell
+#Chrome gnome shell
+##Might not be needed
+#sudo dnf copr -y enable region51/chrome-gnome-shell
+#sudo dnf install -y chrome-gnome-shell
 #Extension Manager
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.mattjakeman.ExtensionManager
