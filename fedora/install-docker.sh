779f97e1373fcbc99ee1a570c1c7206c912d6e56
jperdomo <jperdomo3@gmail.com>
2022-10-02 14:52:14 -0400

    non sudo docker cmds

--- fedora/install-docker.sh
+++ fedora/install-docker.sh
@@ -6,5 +6,9 @@ https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl enable docker
+#User Config
+sudo usermod -aG docker $USER
 #Hello World
 sudo docker run hello-world
+#Reboor prompt
+echo Reboot required for non sudo docker commands!
